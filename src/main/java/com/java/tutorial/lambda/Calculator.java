fd324fc4293a2295589d940c19e6783a252bb342
qiboo <592755073@qq.com>
2018-11-27 20:57:55 +0800

    Calculator类

---
+++ src/main/java/com/java/tutorial/lambda/Calculator.java
@@ -0,0 +1,29 @@
+package com.java.tutorial.lambda;
+
+/**
+ * @author qiboo
+ * 2018-11-27
+ * 20:56
+ */
+public class Calculator {
+
+    interface IntegerMath {
+        int operation(int a, int b);
+    }
+
+    public int operateBinary(int a, int b, IntegerMath op) {
+        return op.operation(a, b);
+    }
+
+    public static void main(String... args) {
+
+        Calculator myApp = new Calculator();
+        IntegerMath addition = (a, b) -> a + b;
+        IntegerMath subtraction = (a, b) -> a - b;
+        System.out.println("40 + 2 = " +
+                myApp.operateBinary(40, 2, addition));
+        System.out.println("20 - 10 = " +
+                myApp.operateBinary(20, 10, subtraction));
+    }
+
+}
