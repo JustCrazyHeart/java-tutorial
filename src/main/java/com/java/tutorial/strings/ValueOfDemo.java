a01d436495642e1b3497af0f5f379aba72f80610
qiboo <592755073@qq.com>
2019-01-09 17:56:09 +0800

    ValueOfDemo

---
+++ src/main/java/com/java/tutorial/strings/ValueOfDemo.java
@@ -0,0 +1,35 @@
+package com.java.tutorial.strings;
+
+/**
+ * @author qiboo
+ * 2019-01-09
+ * 17:55
+ */
+public class ValueOfDemo {
+
+    public static void main(String[] args) {
+
+        // this program requires two 
+        // arguments on the command line
+        if (args.length == 2) {
+            // convert strings to numbers
+            float a = (Float.valueOf(args[0])).floatValue();
+            float b = (Float.valueOf(args[1])).floatValue();
+
+            // do some arithmetic
+            System.out.println("a + b = " +
+                    (a + b));
+            System.out.println("a - b = " +
+                    (a - b));
+            System.out.println("a * b = " +
+                    (a * b));
+            System.out.println("a / b = " +
+                    (a / b));
+            System.out.println("a % b = " +
+                    (a % b));
+        } else {
+            System.out.println("This program " +
+                    "requires two command-line arguments.");
+        }
+    }
+}
