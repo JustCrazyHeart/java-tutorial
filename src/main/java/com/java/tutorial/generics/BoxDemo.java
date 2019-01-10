901de23f7aa0ed751ad8740d0120e64186ec3247
qiboo <592755073@qq.com>
2019-01-10 17:08:06 +0800

    BoxDemo

---
+++ src/main/java/com/java/tutorial/generics/BoxDemo.java
@@ -0,0 +1,35 @@
+package com.java.tutorial.generics;
+
+/**
+ * @author qiboo
+ * 2019-01-10
+ * 17:07
+ */
+public class BoxDemo {
+
+    public static <U> void addBox(U u, java.util.List<Box<U>> boxes) {
+        Box<U> box = new Box<>();
+        box.set(u);
+        boxes.add(box);
+    }
+
+    public static <U> void outputBoxes(java.util.List<Box<U>> boxes) {
+        int counter = 0;
+        for (Box<U> box: boxes) {
+            U boxContents = box.get();
+            System.out.println(
+                    "Box #" + counter + " contains [" +
+                            boxContents.toString() + "]");
+            counter++;
+        }
+    }
+
+    public static void main(String[] args) {
+        java.util.ArrayList<Box<Integer>> listOfIntegerBoxes =
+                new java.util.ArrayList<>();
+        BoxDemo.<Integer>addBox(Integer.valueOf(10), listOfIntegerBoxes);
+        BoxDemo.addBox(Integer.valueOf(20), listOfIntegerBoxes);
+        BoxDemo.addBox(Integer.valueOf(30), listOfIntegerBoxes);
+        BoxDemo.outputBoxes(listOfIntegerBoxes);
+    }
+}
