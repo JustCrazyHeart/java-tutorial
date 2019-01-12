e4fff698eae318214346ecbf3d5823390e936472
qiboo <592755073@qq.com>
2019-01-12 16:14:23 +0800

    Root2

---
+++ src/main/java/com/java/tutorial/basicio/Root2.java
@@ -0,0 +1,16 @@
+package com.java.tutorial.basicio;
+
+/**
+ * @author qiboo
+ * 2019-01-12
+ * 16:12
+ */
+public class Root2 {
+
+    public static void main(String[] args) {
+        int i = 2;
+        double r = Math.sqrt(i);
+
+        System.out.format("The square root of %d is %f.%n", i, r);
+    }
+}
