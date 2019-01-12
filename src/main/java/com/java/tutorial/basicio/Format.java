6ba5bf8a575b94b73c6f5af5ff1d4c76e0f5ad45
qiboo <592755073@qq.com>
2019-01-12 16:32:17 +0800

    Format

---
+++ src/main/java/com/java/tutorial/basicio/Format.java
@@ -0,0 +1,13 @@
+package com.java.tutorial.basicio;
+
+/**
+ * @author qiboo
+ * 2019-01-12
+ * 16:31
+ */
+public class Format {
+
+    public static void main(String[] args) {
+        System.out.format("%f, %1$+020.10f %n", Math.PI);
+    }
+}
