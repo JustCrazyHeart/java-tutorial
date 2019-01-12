9912bcdf4a20a6aa578ff6f9b9c760a95070b83d
qiboo <592755073@qq.com>
2019-01-12 15:47:32 +0800

    Root

---
+++ src/main/java/com/java/tutorial/basicio/Root.java
@@ -0,0 +1,24 @@
+package com.java.tutorial.basicio;
+
+/**
+ * @author qiboo
+ * 2019-01-12
+ * 15:45
+ */
+public class Root {
+
+    public static void main(String[] args) {
+        int i = 2;
+        double r = Math.sqrt(i);
+
+        System.out.print("The square root of ");
+        System.out.print(i);
+        System.out.print(" is ");
+        System.out.print(r);
+        System.out.println(".");
+
+        i = 5;
+        r = Math.sqrt(i);
+        System.out.println("The square root of " + i + " is " + r + ".");
+    }
+}
