7866c7c74a4a5ec544fd17fc0983c597a5a73bc2
qiboo <592755073@qq.com>
2019-03-14 17:02:39 +0800

    Echo

---
+++ src/main/java/com/java/tutorial/platformenv/Echo.java
@@ -0,0 +1,14 @@
+package com.java.tutorial.platformenv;
+
+/**
+ * @author qiboo
+ * 2019-03-14
+ * 17:01
+ */
+public class Echo {
+    public static void main (String[] args) {
+        for (String s: args) {
+            System.out.println(s);
+        }
+    }
+}
